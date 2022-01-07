af62a2c025974ebc1837348b38caac7e7bed30f2
Jonathan Lee <mineroller@hotmail.com>
2022-01-07 17:58:34 +0900

    Add project files.

---
+++ VxDataSourceViewer/frmMain.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace VxDataSourceViewer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lstDataSources = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtVxCoreIP = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblConnectionResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtVxCoreIP);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(1121, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "VxPro / VxCore 로그인";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(310, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(557, 32);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(401, 32);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(148, 29);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(643, 32);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(194, 29);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(860, 21);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(240, 43);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "연결";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lstDataSources
+            // 
+            this.lstDataSources.FormattingEnabled = true;
+            this.lstDataSources.ItemHeight = 21;
+            this.lstDataSources.Location = new System.Drawing.Point(25, 184);
+            this.lstDataSources.Name = "lstDataSources";
+            this.lstDataSources.Size = new System.Drawing.Size(384, 466);
+            this.lstDataSources.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "번호";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(452, 184);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(651, 469);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "선택된 카메라 (DataSource) 정보";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(130, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label5.Location = new System.Drawing.Point(130, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 21);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "이름";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label7.Location = new System.Drawing.Point(130, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(28, 162);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 21);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "UUID";
+            // 
+            // txtVxCoreIP
+            // 
+            this.txtVxCoreIP.Location = new System.Drawing.Point(100, 32);
+            this.txtVxCoreIP.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtVxCoreIP.Name = "txtVxCoreIP";
+            this.txtVxCoreIP.Size = new System.Drawing.Size(167, 29);
+            this.txtVxCoreIP.TabIndex = 6;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 32);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(84, 21);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Vx 서버 IP";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(32, 261);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(597, 193);
+            this.listBox1.TabIndex = 8;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(28, 228);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(114, 21);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "DataInterfaces";
+            // 
+            // lblConnectionResult
+            // 
+            this.lblConnectionResult.AutoSize = true;
+            this.lblConnectionResult.Font = new System.Drawing.Font("Malgun Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblConnectionResult.Location = new System.Drawing.Point(20, 131);
+            this.lblConnectionResult.Name = "lblConnectionResult";
+            this.lblConnectionResult.Size = new System.Drawing.Size(154, 25);
+            this.lblConnectionResult.TabIndex = 4;
+            this.lblConnectionResult.Text = "(서버 접속 정보)";
+            this.lblConnectionResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 683);
+            this.Controls.Add(this.lblConnectionResult);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lstDataSources);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VxSDK DataSource Explorer";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstDataSources;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtVxCoreIP;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblConnectionResult;
+    }
+}
+
---
+++ VxDataSourceViewer/frmMain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using VxSdkNet;
+
+namespace VxDataSourceViewer
+{
+    public partial class frmMain : Form
+    {
+        public static VXSystem testSystem { get; set; } //  VideoXpert 시스템 (서버) 오브젝트       
+        public static string wk_LicenseString = "DCovPywTKiY5LgolLiYsKCI/MywlBRUTdxAAD24BHw0YFQ0="; // INT-JENSON 에 매치되는 SDK 라이센스 키
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            txtVxCoreIP.Enabled = false;
+            txtUsername.Enabled = false;
+            txtPassword.Enabled = false;
+
+            // 서버 오브젝트를 생성하고 연결합니다
+
+            testSystem = new VXSystem(txtVxCoreIP.Text, Convert.ToInt32("443"), true, wk_LicenseString);
+            try
+            {
+
+                var result = testSystem.Login(txtUsername.Text, txtPassword.Text);
+
+                // SDK 라이센스 적용 확인 및 트라이얼 기간인경우 경고 처리
+
+                if (result == Results.Value.SdkLicenseGracePeriodActive)
+                {
+                    DateTime expirationTime = testSystem.GraceLicenseExpirationTime;
+
+                    string _expDate = expirationTime.ToLocalTime().ToShortDateString();
+                    string _expTime = expirationTime.ToLocalTime().ToShortTimeString();
+
+
+                    MessageBox.Show("SDK 트라이얼 라이센스를 사용중이며" + _expDate + " " + _expTime + "에 만료됩니다.", "SDK 라이센스 필요", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else if (result != Results.Value.OK)
+                {
+                    testSystem.Dispose();
+                    if (result == Results.Value.SdkLicenseGracePeriodExpired)
+                        MessageBox.Show("SDK 트라이얼 라이센스가 만료되었습니다. 올바른 라이센스를 적용해주세요", "SDK 라이센스 경고", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    else
+                        MessageBox.Show("로그인 오류 발생: " + result, "시스템 로그인 오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("예외 발생: " + ex.Message, "예외 발생", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // 접속이 완료되면 필요한 오브젝트 생성
+
+            User user = testSystem.Currentuser;
+            Device sysDevice = testSystem.HostDevice;
+
+            List<DataSource> testDataSources = testSystem.DataSources;
+
+            foreach (DataSource ds in testDataSources)
+            {
+                lstDataSources.Items.Add(ds.Name);
+            }
+
+            lblConnectionResult.Text = "Vx 시스템 [" + sysDevice.Name + "] 에 [" + user.Name + "] 으로 접속 성공";
+        }
+    }
+}
